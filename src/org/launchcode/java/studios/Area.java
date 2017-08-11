79015129656f5e2dd8576c98022dc995dd80bb20
neex <neex.io@protonmail.com>
2017-08-10 20:17:39 -0700

    Implement do/while loop.

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -9,17 +9,14 @@ public class Area {
         double area;
         Scanner in;
 
-        in = new Scanner(System.in);
-        System.out.println("Enter a radius:");
-        r = in.nextDouble();
+
+        do {
+            in = new Scanner(System.in);
+            System.out.println("Enter a valid radius:");
+            r = in.nextDouble();
+        } while (r <= 0);
 
         area = pi * r * r;
-
-        if (r <= 0 ) {
-            System.out.println("The radius cannot be negative or zero. At least not on Planet Earth.");
-            System.out.print("Goodbye!");
-        } else {
-            System.out.println("The area of a circle of radius " + r + " is: " + area);
-        }
+        System.out.println("The area of a circle of radius " + r + " is: " + area);
     }
 }
