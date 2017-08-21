d43b7864b7ba7b6d5b871fadd7048361ba992de0
neex-io <code@neex.io>
2017-08-20 19:39:56 -0700

    Add steps to CountingChars

---
+++ src/org/launchcode/java/studios/CountingChars.java
@@ -0,0 +1,14 @@
+package org.launchcode.java.studios;
+
+public class CountingChars {
+    public static void main(String[] args) {
+        // 1. get a string to test
+        // 2. loop through the string one at a time
+        // 3. update counter if present
+        // You can turn a String object into an array of characters using:
+        // char[] charactersInString = myString.toCharArray();
+        // sort the characters ?
+
+
+    }
+}
